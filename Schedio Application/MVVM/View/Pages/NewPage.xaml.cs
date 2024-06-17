69e46e9c63d6bfedecc2abacc764635b505afea6
Jelmar <jelmarmercado@gmail.com>
2024-06-17 16:15:26 +0800

    Added changing icons on New tabcontrol items

--- Schedio Application/MVVM/View/Pages/NewPage.xaml.cs
+++ Schedio Application/MVVM/View/Pages/NewPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,30 @@ namespace Schedio_Application.MVVM.View.Pages
         public NewPage()
         {
             InitializeComponent();
+        }
+
+        private void tabCntrl_NewPage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            changeTabImage();
+        }
+
+        private void changeTabImage()
+        {
+            TabItem[] tabItems = { tabItem_Personnel, tabItem_Rooms, tabItem_Sections, tabItem_Time };
+
+            for (int i = 0; i < tabItems.Length; i++)
+            {
+                String tabItemName = tabItems[i].Name.ToString().Substring(8, tabItems[i].Name.Length - 8);
+
+                if (tabItems[i].IsSelected)
+                {
+                    ((Image)tabItems[i].FindName("img_" + tabItemName)).Source = new BitmapImage(new Uri(String.Format("pack://application:,,,/Schedio Application;component/Resources/Images/{0}-filled.png", tabItemName.ToLower())));
+                }
+                else
+                {
+                    ((Image)tabItems[i].FindName("img_" + tabItemName)).Source = new BitmapImage(new Uri(String.Format("pack://application:,,,/Schedio Application;component/Resources/Images/{0}.png", tabItemName.ToLower())));
+                }
+            }
         }
     }
 }
