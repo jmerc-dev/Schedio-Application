2553e60592789aa93b900f9ba3c0c7979643b629
Jelmar <jelmarmercado@gmail.com>
2024-06-18 14:07:45 +0800

    Added 'Add' button and improved layout on New Page.

--- Schedio Application/MVVM/View/Pages/NewPage.xaml.cs
+++ Schedio Application/MVVM/View/Pages/NewPage.xaml.cs
@@ -29,6 +29,8 @@ namespace Schedio_Application.MVVM.View.Pages
         private void tabCntrl_NewPage_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             changeTabImage();
+            ChangeAddButton(btn_Add);
+            
         }
 
         private void changeTabImage()
@@ -46,6 +48,31 @@ namespace Schedio_Application.MVVM.View.Pages
                 else
                 {
                     ((Image)tabItems[i].FindName("img_" + tabItemName)).Source = new BitmapImage(new Uri(String.Format("pack://application:,,,/Schedio Application;component/Resources/Images/{0}.png", tabItemName.ToLower())));
+                }
+            }
+        }
+
+        // Add button will be manipulated according to selected Tab Item
+        private void ChangeAddButton(Button button)
+        {
+            TabItem[] tabItems = { tabItem_Personnel, tabItem_Rooms, tabItem_Sections, tabItem_Time };
+            Image buttonImage = ((Image)button.FindName("img_ButtonAdd"));
+
+            for (int i = 0;i < tabItems.Length;i++)
+            {
+                String tabItemName = tabItems[i].Name.ToString().Substring(8, tabItems[i].Name.Length - 8);
+       
+                if (tabItems[i].IsSelected)
+                {
+                    if (tabItemName.Equals("Time"))
+                    {
+                        button.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        button.Visibility = Visibility.Visible;
+                        buttonImage.Source = new BitmapImage(new Uri(String.Format("pack://application:,,,/Schedio Application;component/Resources/Images/{0}-add.png", tabItemName.ToLower())));
+                    }
                 }
             }
         }
