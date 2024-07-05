97f0d5415aa82790684a68242e66d372373dce82
Jelmar <jelmarmercado@gmail.com>
2024-07-05 15:30:22 +0800

    bugfix: counter textbox isn't limited to digits only
    - all characters aside from digits from 0-99 are restricted
    - increment and decrement buttons handle exceptions when the counter textbox is empty

--- Schedio Application/MVVM/View/UserControls/Counter.xaml.cs
+++ Schedio Application/MVVM/View/UserControls/Counter.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,9 @@ namespace Schedio_Application.MVVM.View.UserControls
     /// </summary>
     public partial class Counter : UserControl
     {
+
+        private string oldValue = "0";
+
         public Counter()
         {
             InitializeComponent();
@@ -27,25 +31,56 @@ namespace Schedio_Application.MVVM.View.UserControls
 
         private void btn_Add_Click(object sender, RoutedEventArgs e)
         {
-            int num = Convert.ToInt32(tb_Num.Text);
-            if (num < 99)
+            try
             {
-                tb_Num.Text = (num + 1).ToString();
+                int num = Convert.ToInt32(tb_Num.Text);
+                if (num < 99)
+                {
+                    tb_Num.Text = (num + 1).ToString();
+                }
             }
+            catch
+            {
+                tb_Num.Text = "1";
+            }
+
         }
 
         private void btn_Sub_Click(object sender, RoutedEventArgs e)
         {
-            int num = Convert.ToInt32(tb_Num.Text);
-            if (num > 0)
+            try
             {
-                tb_Num.Text = (num - 1).ToString();
+                int num = Convert.ToInt32(tb_Num.Text);
+                if (num > 0)
+                {
+                    tb_Num.Text = (num - 1).ToString();
+                }
+            } catch
+            {
+                tb_Num.Text = "1";
             }
+
         }
 
         private void tb_Num_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (tb_Num.Text.Equals(String.Empty))
+            {
+                oldValue = String.Empty;
+                return;
+            }
 
+            string pattern = @"^\d{1,2}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(tb_Num.Text))
+            {
+                oldValue = tb_Num.Text;
+            }
+            else
+            {
+                tb_Num.Text = oldValue;
+                tb_Num.CaretIndex = oldValue.Length;
+            }
         }
     }
 }
