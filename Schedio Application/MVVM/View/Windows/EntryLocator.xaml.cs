a9e219c0bda2e0afd256d871a71033602a9606e0
jmerc-dev <jelmarmercado@gmail.com>
2025-04-13 17:55:34 +0800

    added: entrylocator window with title and 1st row items

---
+++ Schedio Application/MVVM/View/Windows/EntryLocator.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Schedio_Application.MVVM.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for EntryLocator.xaml
+    /// </summary>
+    public partial class EntryLocator : Window
+    {
+        public EntryLocator()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+
+                }
+                else
+                {
+                    this.DragMove();
+                }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- Schedio Application/MVVM/View/Windows/Workshop.xaml.cs
+++ Schedio Application/MVVM/View/Windows/Workshop.xaml.cs
@@ -41,6 +41,7 @@ namespace Schedio_Application.MVVM.View.Windows
         private FileLoader _FileLoad;
         private ClassSection? _SelectedSection;
         private FileSave _FileSave = new FileSave();
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public ClassSection? SelectedSection
         {
@@ -61,8 +62,6 @@ namespace Schedio_Application.MVVM.View.Windows
         {
             get { return "No Chosen Section"; }
         }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
@@ -97,7 +96,7 @@ namespace Schedio_Application.MVVM.View.Windows
             Loaded += (sender, e) =>
             {
                 this.DataContext = this;
-                //AddDummyData();
+                new DummyData(true, RoomTypes, Rooms, Personnel, Sections);
             };
 
             Sections.CollectionChanged += new NotifyCollectionChangedEventHandler(section_CollectionChanged);
@@ -139,10 +138,6 @@ namespace Schedio_Application.MVVM.View.Windows
             {
                 this.DataContext = this;
                 LoadEntries(Subject.SubjectEntries);
-                //AddDummyData();
-                RoomTypes.Add(new RoomType("Classic"));
-                RoomTypes.Add(new RoomType("Lab"));
-                RoomTypes.Add(new RoomType("Court"));
             };
 
             Closing += (sender, e) =>
@@ -159,174 +154,6 @@ namespace Schedio_Application.MVVM.View.Windows
                 getDayTable(entry.DayAssigned).addEntry(entry);
 
             return true;
-        }
-
-        // Subject allocation CRUD
-
-        // Dummy Data
-        private void AddDummyData()
-        {
-            //RoomTypes = new ObservableCollection<RoomType>();
-            RoomTypes.Add(new RoomType("Classic"));
-            RoomTypes.Add(new RoomType("Lab"));
-            RoomTypes.Add(new RoomType("Court"));
-
-            Rooms.Add(new Room("101", RoomTypes[0]));
-            Rooms.Add(new Room("102", RoomTypes[1]));
-            Rooms.Add(new Room("103", RoomTypes[2]));
-            Rooms.Add(new Room("104", RoomTypes[1]));
-            Rooms.Add(new Room("105", RoomTypes[1]));
-            Rooms.Add(new Room("106", RoomTypes[1]));
-            Rooms.Add(new Room("107", RoomTypes[1]));
-            Rooms.Add(new Room("108", RoomTypes[1]));
-            Rooms.Add(new Room("109", RoomTypes[2]));
-            Rooms.Add(new Room("110", RoomTypes[1]));
-            Rooms.Add(new Room("201", RoomTypes[2]));
-            Rooms.Add(new Room("202", RoomTypes[1]));
-            Rooms.Add(new Room("203", RoomTypes[0]));
-            Rooms.Add(new Room("204", RoomTypes[0]));
-            Rooms.Add(new Room("205", RoomTypes[0]));
-
-            Person[] people = [
-                new Person { Name = "Jose Protacio Rizal", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "02:00 PM"},
-                new Person { Name = "Emilio Aguinaldo", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "05:00 PM"},
-                new Person { Name = "Apolinario Mabini", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "03:00 PM"},
-                new Person { Name = "Arthur McArthur", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "01:00 PM"},
-                new Person { Name = "Andres Bonifacio", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "07:00 PM"},
-                new Person { Name = "Lapu Lapu", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "05:00 PM"},
-                new Person { Name = "Apo Whang-od", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "02:00 PM"},
-                new Person { Name = "Tomoyuki Yamashita", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "01:00 PM"},
-                new Person { Name = "Gabriela Silang", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "09:00 PM"},
-                new Person { Name = "Bong Bong Marcos", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "08:00 PM"},
-                new Person { Name = "Benigno Aquino", IsConstant = true, ConstTime_Start = "12:00 AM", ConstTime_End = "03:00 PM"}
-                ];
-
-            foreach (Person p in people)
-            {
-                p.Initialize();
-            }
-
-            people[0].SetAvailableDay(DayOfWeek.Saturday, true);
-            people[0].SetAvailableDay(DayOfWeek.Monday, true);
-
-            people[1].SetAvailableDay(DayOfWeek.Monday, true);
-            people[1].SetAvailableDay(DayOfWeek.Tuesday, true);
-            people[1].SetAvailableDay(DayOfWeek.Wednesday, true);
-
-            people[2].SetAvailableDay(DayOfWeek.Tuesday, true);
-            people[2].SetAvailableDay(DayOfWeek.Wednesday, true);
-
-            people[3].SetAvailableDay(DayOfWeek.Tuesday, true);
-
-            people[4].SetAvailableDay(DayOfWeek.Thursday, true);
-            people[4].SetAvailableDay(DayOfWeek.Friday, true);
-
-            people[5].SetAvailableDay(DayOfWeek.Monday, true);
-            people[5].SetAvailableDay(DayOfWeek.Friday, true);
-
-            people[6].SetAvailableDay(DayOfWeek.Monday, true);
-            people[6].SetAvailableDay(DayOfWeek.Tuesday, true);
-            people[6].SetAvailableDay(DayOfWeek.Thursday, true);
-            people[6].SetAvailableDay(DayOfWeek.Saturday, true);
-
-            people[7].SetAvailableDay(DayOfWeek.Tuesday, true);
-            people[7].SetAvailableDay(DayOfWeek.Wednesday, true);
-            people[7].SetAvailableDay(DayOfWeek.Friday, true);
-
-            people[8].SetAvailableDay(DayOfWeek.Monday, true);
-            people[8].SetAvailableDay(DayOfWeek.Thursday, true);
-            people[8].SetAvailableDay(DayOfWeek.Saturday, true);
-
-            people[9].SetAvailableDay(DayOfWeek.Tuesday, true);
-
-            people[10].SetAvailableDay(DayOfWeek.Monday, true);
-            people[10].SetAvailableDay(DayOfWeek.Saturday, true);
-            
-
-            foreach (Person person in people)
-            {
-                Personnel.Add(person);
-            }
-
-
-            ClassSection[] DummySection = new ClassSection[10];
-            for (int i = 0; i < DummySection.Length; i++)
-            {
-                string[] courses = ["IT", "CS", "HRS", "CE", "P"];
-                DummySection[i] = new ClassSection();
-                if (i < 2)
-                    DummySection[i].Name =  $"{courses[0]}{i}0A";
-                else if (i < 4)
-                    DummySection[i].Name = $"{courses[1]}{i}0A";
-                else if (i < 6)
-                    DummySection[i].Name = $"{courses[2]}{i}0A";
-                else if (i < 8)
-                    DummySection[i].Name = $"{courses[3]}{i}0A";
-                else
-                    DummySection[i].Name = $"{courses[4]}{i}0A";
-
-                Random rnd = new Random();
-                
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "NSTP II",
-                    AssignedPerson = people[0],
-                    RoomType = RoomTypes[0],
-                    Units = 1,
-                    OwnerSection = DummySection[i]
-                });
-
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "Computer Programming",
-                    AssignedPerson = people[1],
-                    RoomType = RoomTypes[0],
-                    Units = 3,
-                    OwnerSection = DummySection[i]
-                });
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "Computer Programming Lab",
-                    AssignedPerson = people[2],
-                    RoomType = RoomTypes[1],
-                    Units = 2,
-                    OwnerSection = DummySection[i]
-                });
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "Entrepreneurship",
-                    AssignedPerson = people[3],
-                    RoomType = RoomTypes[0],
-                    Units = 2,
-                    OwnerSection = DummySection[i]
-                });
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "Automata Theory",
-                    AssignedPerson = people[4],
-                    RoomType = RoomTypes[0],
-                    Units = 3,
-                    OwnerSection = DummySection[i]
-                });
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "PE",
-                    AssignedPerson = people[5],
-                    RoomType = RoomTypes[2],
-                    Units = 2,
-                    OwnerSection = DummySection[i]
-                });
-                DummySection[i].Subjects.Add(new Subject
-                {
-                    Name = "Design and Analysis of Algorithms",
-                    AssignedPerson = people[6],
-                    RoomType = RoomTypes[0],
-                    Units = 3,
-                    OwnerSection = DummySection[i]
-                });
-                Sections.Add(DummySection[i]);
-            }
-
         }
 
         // Subjects panel
@@ -357,7 +184,6 @@ namespace Schedio_Application.MVVM.View.Windows
             {
                 // Save selected Section
                 SelectedSection = sectionExplorer.SelectedSection;
-                //lv_SelectedSectionSubjects.ItemsSource = SelectedSection.Subjects;
             }
         }
 
@@ -367,6 +193,11 @@ namespace Schedio_Application.MVVM.View.Windows
             Subject.SubjectEntries.Clear();
 
         }
+
+        private void btn_Find_Click(object sender, RoutedEventArgs e)
+        {
+            new EntryLocator().Show();
+        }
     }
 
 
@@ -375,9 +206,15 @@ namespace Schedio_Application.MVVM.View.Windows
     {
         private FileLoader file;
         private FullDataWrapper fullDataWrapper;
-
         private ObservableCollection<Room> TempRooms;
         private ObservableCollection<RoomType> RoomTypes = new ObservableCollection<RoomType>();
+        private ObservableCollection<Person> Personnel;
+        private ObservableCollection<Person> TempPersonnel;
+
+        private ObservableCollection<ClassSection> Sections;
+        private ObservableCollection<ClassSection> TempSections;
+
+        private WarningConfirmation? warningModal;
 
         public static ObservableCollection<Room> Rooms 
         { 
@@ -388,14 +225,6 @@ namespace Schedio_Application.MVVM.View.Windows
         {
             get => fullDataWrapper;
         }
-
-        private ObservableCollection<Person> Personnel;
-        private ObservableCollection<Person> TempPersonnel;
-
-        private ObservableCollection<ClassSection> Sections;
-        private ObservableCollection<ClassSection> TempSections;
-
-        private WarningConfirmation? warningModal;
 
         private void tabCtrl_DataManager_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -599,7 +428,7 @@ namespace Schedio_Application.MVVM.View.Windows
             return null;
         }
 
-        private TimeTable? getDayTable(DayOfWeek? day)
+        private TimeTable getDayTable(DayOfWeek? day)
         {
             switch (day)
             {
@@ -617,7 +446,7 @@ namespace Schedio_Application.MVVM.View.Windows
                     return tt_Friday;
                 case DayOfWeek.Saturday:
                     return tt_Saturday;
-                default: return null;
+                default: throw new NullReferenceException();
                     
             }
         }
